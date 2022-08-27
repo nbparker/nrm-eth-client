02c70dc6f45de9f2467d2dd34ea551dfb5d350fc
nathan <pnathan117@gmail.com>
2022-08-27 13:55:38 +0100

    command line server address

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"io"
 	"log"
 	"time"
@@ -12,12 +13,18 @@ import (
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
+var (
+	serverAddr = flag.String("addr", "localhost:50051", "The server address in the format of host:port")
+)
+
 func main() {
+	flag.Parse()
+
 	var opts []grpc.DialOption
 	// Run without transport creds
 	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
-	conn, err := grpc.Dial("nrm-eth_nrm-eth-server_1:50051", opts...)
+	conn, err := grpc.Dial(*serverAddr, opts...)
 	if err != nil {
 		log.Fatalf("Failed to dial: %v", err)
 	}
