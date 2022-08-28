2fbb799a8ff4f01f6d60d652333b4f7a3e38439b
nathan <pnathan117@gmail.com>
2022-08-28 12:02:38 +0100

    refactor out GetUpdates

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -2,12 +2,8 @@ package client
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 	"io"
 	"log"
-	"os"
-	"path/filepath"
 	"time"
 
 	"github.com/nbparker/nrm-eth-client/pkg/proto/nrm"
@@ -35,7 +31,7 @@ func (c *NRMClient) RunStore() {
 
 	// Send protos to store
 	updates := make(chan *nrm.GenericUpdate)
-	go c.getUpdates(updates)
+	go GetUpdates(c.UpdatesFolderPath, updates)
 	for update := range updates {
 		if err := stream.Send(update); err != nil {
 			log.Fatalf("client.Store: stream.Send(%v) failed: %v", update, err)
@@ -59,51 +55,5 @@ func (c *NRMClient) handleSummaries() {
 			"Received summary. Success: %v, Attempts: %d, Last Attempt: %v (error: %s)",
 			in.Success, in.Attempts, in.LastAttemptedAt, in.FailureMessage,
 		)
-	}
-}
-
-func (c *NRMClient) getUpdates(updates chan *nrm.GenericUpdate) {
-	// No updates if no folder name
-	if c.UpdatesFolderPath == "" {
-		log.Fatal("No folder specified so no updates to send")
-	}
-
-	files, err := os.ReadDir(c.UpdatesFolderPath)
-	if err != nil {
-		log.Fatalf("Failed to read dir: %v", err)
-	}
-
-	// Iterate files in updates folder, adding updates
-	for _, file := range files {
-		path := filepath.Join(c.UpdatesFolderPath, file.Name())
-		log.Printf("Reading file: %s", path)
-
-		data, err := os.ReadFile(path)
-		if err != nil {
-			log.Fatalf("Failed to read updates file: %v", err)
-		}
-
-		// Attempt to marshall individual json object
-		var _update *nrm.GenericUpdate
-		if err := json.Unmarshal(data, &_update); err == nil {
-			updates <- _update
-
-			time.Sleep(time.Millisecond * 200) // TODO remove
-			continue
-		}
-
-		// Attempt to marshall list of json objects
-		var _updates []*nrm.GenericUpdate
-		if err := json.Unmarshal(data, &_updates); err == nil {
-			for _, _update := range _updates {
-				updates <- _update
-
-				time.Sleep(time.Millisecond * 200) // TODO remove
-			}
-			continue
-		}
-
-		// Log but continue to next file
-		fmt.Printf("Failed to read json from file: %s", path)
 	}
 }
---
+++ pkg/client/updates.go
@@ -0,0 +1,58 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/nbparker/nrm-eth-client/pkg/proto/nrm"
+)
+
+func GetUpdates(folderPath string, updates chan *nrm.GenericUpdate) {
+	// No updates if no folder name
+	if folderPath == "" {
+		log.Fatal("No folder specified so no updates to send")
+	}
+
+	files, err := os.ReadDir(folderPath)
+	if err != nil {
+		log.Fatalf("Failed to read dir: %v", err)
+	}
+
+	// Iterate files in updates folder, adding updates
+	for _, file := range files {
+		path := filepath.Join(folderPath, file.Name())
+		log.Printf("Reading file: %s", path)
+
+		data, err := os.ReadFile(path)
+		if err != nil {
+			log.Fatalf("Failed to read updates file: %v", err)
+		}
+
+		// Attempt to marshall individual json object
+		var _update *nrm.GenericUpdate
+		if err := json.Unmarshal(data, &_update); err == nil {
+			updates <- _update
+
+			time.Sleep(time.Millisecond * 200) // TODO remove
+			continue
+		}
+
+		// Attempt to marshall list of json objects
+		var _updates []*nrm.GenericUpdate
+		if err := json.Unmarshal(data, &_updates); err == nil {
+			for _, _update := range _updates {
+				updates <- _update
+
+				time.Sleep(time.Millisecond * 200) // TODO remove
+			}
+			continue
+		}
+
+		// Log but continue to next file
+		fmt.Printf("Failed to read json from file: %s", path)
+	}
+}
