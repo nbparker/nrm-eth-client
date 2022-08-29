1191c93cdfc7875ff3abbbf85b4355a694937848
nathan <pnathan117@gmail.com>
2022-08-29 15:04:15 +0100

    refactor to struct fields

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -14,41 +14,57 @@ type NRMClient struct {
 	Client            nrm.NaturalResourceManagementClient
 	UpdatesFolderPath string
 
-	stream nrm.NaturalResourceManagement_StoreClient
+	stream  nrm.NaturalResourceManagement_StoreClient
+	errors  chan error
+	updates chan *nrm.GenericUpdate
+	waitc   chan struct{}
+	cancel  context.CancelFunc
 }
 
 // RunStore finds updates and sends to server
 func (c *NRMClient) RunStore() error {
+	c.initialise()
+	defer c.cancel()
+
+	go c.handleSummaries()
+	go GetUpdates(c.UpdatesFolderPath, c.updates, c.errors)
+	err := c.sendUpdates()
+	c.stream.CloseSend()
+
+	<-c.waitc
+	return err
+}
+
+func (c *NRMClient) initialise() error {
 	// Connect to stream
 	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
-	defer cancel()
 	stream, err := c.Client.Store(ctx)
 	if err != nil {
 		return fmt.Errorf("client.Store failed: %w", err)
 	}
+
 	c.stream = stream
-
-	updates := make(chan *nrm.GenericUpdate)
-	errs := make(chan error)
-
-	go c.handleSummaries(errs)
-	go GetUpdates(c.UpdatesFolderPath, updates, errs)
-	return c.sendUpdates(updates, errs)
+	c.updates = make(chan *nrm.GenericUpdate)
+	c.errors = make(chan error)
+	c.waitc = make(chan struct{})
+	c.cancel = cancel
+	return nil
 }
 
 // sendUpdates to server
 // iterates update and error channels until closed or error
-func (c *NRMClient) sendUpdates(updates chan *nrm.GenericUpdate, errs chan error) error {
+func (c *NRMClient) sendUpdates() error {
+
 	for {
 		select {
-		case update, ok := <-updates:
+		case update, ok := <-c.updates:
 			if !ok {
 				return nil
 			}
 			if err := c.stream.Send(update); err != nil {
 				return fmt.Errorf("client.Store: stream.Send(%v) failed: %w", update, err)
 			}
-		case err, ok := <-errs:
+		case err, ok := <-c.errors:
 			if !ok {
 				return nil
 			}
@@ -57,17 +73,16 @@ func (c *NRMClient) sendUpdates(updates chan *nrm.GenericUpdate, errs chan error
 	}
 }
 
-func (c *NRMClient) handleSummaries(errs chan error) {
-	waitc := make(chan struct{})
+func (c *NRMClient) handleSummaries() {
 	for {
 		in, err := c.stream.Recv()
 		if err == io.EOF {
 			// Stream closed
-			close(waitc)
+			close(c.waitc)
 			return
 		}
 		if err != nil {
-			errs <- fmt.Errorf("client.Store recv failed: %w", err)
+			c.errors <- fmt.Errorf("client.Store recv failed: %w", err)
 		}
 		log.Printf(
 			"Received summary. Success: %v, Attempts: %d, Last Attempt: %v (error: %s)",
